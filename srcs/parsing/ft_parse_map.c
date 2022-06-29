7da983c05ee0023597a391b6f31bc12e1c271e6a
Hamza El makhroubi <hel-makh@e3r5p1.1337.ma>
2022-06-29 13:00:51 +0100

    fixed no player/wall/space

--- srcs/parsing/ft_parse_map.c
+++ srcs/parsing/ft_parse_map.c
@@ -6,11 +6,17 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 13:48:05 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/06/24 17:08:13 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/06/29 12:59:54 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+typedef struct s_content {
+	int	space;
+	int	wall;
+	int	player;
+}	t_content;
 
 static int	ft_read_map(t_map *map, int fd)
 {
@@ -41,27 +47,48 @@ static int	ft_read_map(t_map *map, int fd)
 	return (1);
 }
 
+static int	ft_check_component(t_map *map, int i, int j, t_content *content)
+{
+	if (!ft_strchr(" 01NSEW", map->map[i][j]))
+	{
+		printf("Error\nInvalid map: Invalid component [%c]."
+			"\nLine: %d, Column: %d\n", map->map[i][j], i + 1, j + 1);
+		return (0);
+	}
+	if (map->map[i][j] == '0')
+		content->space ++;
+	else if (map->map[i][j] == '1')
+		content->wall ++;
+	else if (map->map[i][j] == 'N' || map->map[i][j] == 'S'
+			|| map->map[i][j] == 'E' || map->map[i][j] == 'W')
+		content->player ++;
+	return (1);
+}
+
 static int	ft_check_map_content(t_map *map)
 {
-	int	i;
-	int	j;
+	t_content	content;
+	int			i;
+	int			j;
 
+	content.space = 0;
+	content.wall = 0;
+	content.player = 0;
 	i = 0;
 	while (map->map[i])
 	{
-		j = 0;
-		while (map->map[i][j])
-		{
-			if (!ft_strchr(" 01NSEW", map->map[i][j]))
-			{
-				printf("Error\nInvalid map: Invalid component [%c]."
-					"\nLine: %d, Column: %d\n", map->map[i][j], i + 1, j + 1);
+		j = -1;
+		while (map->map[i][++j])
+			if (!ft_check_component(map, i, j, &content))
 				return (0);
-			}
-			j ++;
-		}
 		i ++;
 	}
+	if (!content.space)
+		return (printf("Error\nInvalid map: No valid path.\n"), 0);
+	else if (!content.wall)
+		return (printf("Error\nInvalid map: Map not closed.\n"), 0);
+	else if (content.player != 1)
+		return (printf("Error\nInvalid map: Missing player.\n"), 0);
 	return (1);
 }
 
