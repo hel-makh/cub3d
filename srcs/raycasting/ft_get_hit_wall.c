c568a58edcf92b4171e3e132b6987b7468adaea1
Yasser Bensellam <ybensell@e3r7p3.1337.ma>
2022-08-11 13:28:26 +0100

    Added and modified get_hit_wall

--- srcs/raycasting/ft_get_hit_wall.c
+++ srcs/raycasting/ft_get_hit_wall.c
@@ -3,104 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_hit_wall.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ybensell <ybensell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/23 11:06:14 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/08/03 00:11:42 by hel-makh         ###   ########.fr       */
+/*   Created: 2022/08/11 10:27:22 by ybensell          #+#    #+#             */
+/*   Updated: 2022/08/11 10:39:35 by ybensell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static t_coor	ft_get_horiz_hit_wall(t_coor point, double angle)
+static t_coor	ft_intersec_y(t_coor pos, double angle)
 {
-	double	tangent;
-	t_coor	hit_line;
+	t_coor	tmp;
 
-	if (angle > M_PI)
-		tangent = tan((M_PI + M_PI_2) - angle);
+	tmp.x = floor(pos.x);
+	if (angle > M_PI_2 && angle < 3 * M_PI_2)
+		tmp.x += 1;
 	else
-		tangent = tan(angle - M_PI_2);
-	if (isnan(tangent) || errno == HUGE_VALF)
-		tangent = 0;
-	hit_line.x = point.x;
-	hit_line.y = floor(point.y);
-	if (angle > M_PI)
-		hit_line.y += 1;
-	else
-		hit_line.y -= 0.000000000001;
-	hit_line.x += ft_get_distance(point, hit_line) * tangent;
-	return (hit_line);
+		tmp.x -= 0.00000000001;
+	tmp.y = pos.y + (tan(angle) * (tmp.x - pos.x));
+	return (tmp);
 }
 
-static t_coor	ft_get_vertic_hit_wall(t_coor point, double angle)
+static t_coor	ft_intersec_x(t_coor pos, double angle)
 {
-	double	tangent;
-	t_coor	hit_line;
+	t_coor	tmp;
 
-	if (angle > M_PI_2 && angle < M_PI + M_PI_2)
-		tangent = tan(angle - M_PI);
+	tmp.y = floor(pos.y);
+	if (angle > M_PI)
+		tmp.y += 1;
 	else
-		tangent = tan(-angle);
-	if (isnan(tangent) || errno == HUGE_VALF)
-		tangent = 0;
-	hit_line.x = floor(point.x);
-	if (angle > M_PI_2 && angle < M_PI + M_PI_2)
-		hit_line.x += 1;
-	else
-		hit_line.x -= 0.000000000001;
-	hit_line.y = point.y;
-	hit_line.y += ft_get_distance(point, hit_line) * tangent;
-	return (hit_line);
+		tmp.y -= 0.00000000001;
+	tmp.x = pos.x + (tmp.y - pos.y) / tan(angle);
+	return (tmp);
 }
 
-static t_coor	ft_get_direc_hit_wall(t_vars *vars, t_coor start_pos,
-	double angle, t_coor (*ft_hit_wall)(t_coor, double))
+static t_coor	get_intersect_pt(t_vars *vars, t_coor start_pos,
+	double angle, t_coor (*interfunc)(t_coor, double))
 {
-	t_coor	wall;
-	t_coor	dist;
+	int		map_height;
+	t_coor	first_inter;
+	t_coor	delta;
 
-	wall = ft_hit_wall(start_pos, angle);
-	dist = ft_hit_wall(wall, angle);
-	dist.x = dist.x - wall.x;
-	dist.y = dist.y - wall.y;
-	while ((dist.x || dist.y) && (int)wall.y >= 0 && (int)wall.x >= 0
-		&& (int)wall.y < (int)ft_arrlen(vars->map.map)
-		&& (int)wall.x < (int)ft_strlen(vars->map.map[(int)wall.y])
-		&& !ft_strchr(HIT_WALLS, vars->map.map[(int)wall.y][(int)wall.x]))
+	map_height = ft_arrlen(vars->map.map);
+	first_inter = interfunc(start_pos, angle);
+	delta = interfunc(first_inter, angle);
+	delta.x -= first_inter.x;
+	delta.y -= first_inter.y;
+	while ((first_inter.x || first_inter.y)
+		&& first_inter.x > 0 && first_inter.y > 0
+		&& first_inter.y < map_height
+		&& first_inter.x < (int)ft_strlen(vars->map.map[(int)first_inter.y])
+		&& !ft_strchr(HIT_WALLS,
+			vars->map.map[(int)first_inter.y][(int)first_inter.x]))
 	{
-		wall.x += dist.x;
-		wall.y += dist.y;
+		first_inter.x += delta.x;
+		first_inter.y += delta.y;
 	}
-	if ((int)wall.y >= 0 && (int)wall.x >= 0
-		&& (int)wall.y < (int)ft_arrlen(vars->map.map)
-		&& (int)wall.x < (int)ft_strlen(vars->map.map[(int)wall.y])
-		&& !ft_strchr(HIT_WALLS, vars->map.map[(int)wall.y][(int)wall.x]))
-		return (start_pos);
-	return (wall);
+	return (first_inter);
 }
 
 t_coor	ft_get_hit_wall(t_vars *vars, t_coor start_pos,
 	double angle, int *direction)
 {
-	t_coor	h_wall;
-	t_coor	v_wall;
+	t_coor	x_wall;
+	t_coor	y_wall;
 
-	h_wall = ft_get_direc_hit_wall(vars, start_pos,
-			angle, &ft_get_horiz_hit_wall);
-	v_wall = ft_get_direc_hit_wall(vars, start_pos,
-			angle, &ft_get_vertic_hit_wall);
-	if (ft_get_distance(vars->player.pos, h_wall)
-		< ft_get_distance(vars->player.pos, v_wall))
-	{
+	x_wall = get_intersect_pt(vars, start_pos, angle, &ft_intersec_x);
+	y_wall = get_intersect_pt(vars, start_pos, angle, &ft_intersec_y);
+	if (ft_get_distance(start_pos, x_wall) < ft_get_distance(start_pos, y_wall))
+	{		
 		if (direction)
 			*direction = 'h';
-		return (h_wall);
+		return (x_wall);
 	}
 	else
 	{
 		if (direction)
 			*direction = 'v';
-		return (v_wall);
+		return (y_wall);
 	}
 }
