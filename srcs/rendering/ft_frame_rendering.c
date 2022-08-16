10ef905e16bc3c3560fb10d87957ec7bf7dd298d
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-08-16 14:46:33 +0100

    fixed leak caused by fps print

--- srcs/rendering/ft_frame_rendering.c
+++ srcs/rendering/ft_frame_rendering.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 16:44:23 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/08/15 21:53:42 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/08/16 14:44:52 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ static void	ft_fps_counter(t_vars *vars)
 
 int	frame_rendering(t_vars *vars)
 {
+	char*	fps;
+
 	ft_fps_counter(vars);
 	ft_move_player(vars);
 	ft_door_animation(vars);
@@ -55,7 +57,9 @@ int	frame_rendering(t_vars *vars)
 	mlx_clear_window(vars->mlx.mlx, vars->mlx.win);
 	mlx_put_image_to_window(vars->mlx.mlx, vars->mlx.win,
 		vars->mlx.img.img, 0, 0);
+	fps = ft_itoa(vars->mlx.fps);
 	mlx_string_put(vars->mlx.mlx, vars->mlx.win, FPS_X, FPS_Y,
-		ft_create_trgb(0, 0, 255, 0), ft_itoa(vars->mlx.fps));
+		ft_create_trgb(0, 0, 255, 0), fps);
+	fps = ft_free(fps);
 	return (1);
 }
