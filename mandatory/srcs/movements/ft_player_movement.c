893b2e55f1120eb7c3a210fabd8ab94b14190b34
Hamza El makhroubi <hel-makh@e3r7p4.1337.ma>
2022-08-29 16:44:25 +0100

    fix: char * to char

--- mandatory/srcs/movements/ft_player_movement.c
+++ mandatory/srcs/movements/ft_player_movement.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 22:27:23 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/08/25 15:52:08 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/08/29 16:43:40 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,11 @@ void	ft_move_player(t_vars *vars)
 		return ;
 	new_pos.x = vars->player.pos.x
 		+ (vars->player.dir.x * vars->mlx.fspeed * SPEED);
-	if (vars->map.map[(int)vars->player.pos.y][(int)new_pos.x] != "1")
+	if (vars->map.map[(int)vars->player.pos.y][(int)new_pos.x] != '1')
 		vars->player.pos.x = new_pos.x;
 	new_pos.y = vars->player.pos.y
 		+ (vars->player.dir.y * vars->mlx.fspeed * SPEED);
-	if (vars->map.map[(int)new_pos.y][(int)vars->player.pos.x] != "1")
+	if (vars->map.map[(int)new_pos.y][(int)vars->player.pos.x] != '1')
 		vars->player.pos.y = new_pos.y;
 }
 
